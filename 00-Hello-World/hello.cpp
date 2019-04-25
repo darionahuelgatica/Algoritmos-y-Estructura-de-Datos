f8f56e16744e25195a14e64da8c374715c7f4901
darionahuelgatica <dario.20398@gmail.com>
2019-04-25 12:19:31 -0300

    Primer merge
    Primer merge.
    Subo carpetas y archivos (que use para tps 00 y 01 y solo los tenia en mi disco) a Git

---
+++ 00-Hello-World/hello.cpp
@@ -0,0 +1,12 @@
+/*
+    Trabajo Práctico 00: Hello, World!
+    Darío Nahuel Gatica (legajo 163 855 5)
+    21/04/2019
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout <<"Hello, world!";
+    return 0;
+}
---
+++ 01-Adicion/adicion.cpp
@@ -0,0 +1,15 @@
+/*
+*
+*
+*
+*/
+#include <iostream>
+
+int main() {
+    std::cout<<"Bienvenido.\nEste es el asistente de adicion.\nPor favor, ingrese dos numeros.\n";
+    int a = 0;
+    std::cin>>a;
+    int b = 0;
+    std::cin>>b;
+    std::cout<<a<<" + "<<b<<" = "<<a+b;
+}
