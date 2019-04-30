d5ef8c168c5bbaad8cecd2aa2126f6cbb9917246
darionahuelgatica <dario.20398@gmail.com>
2019-04-30 20:28:03 -0300

    Elimino los "printf" y reordeno los "testValue"

--- 02-TiposDeDatos/TiposDeDatos.cpp
+++ 02-TiposDeDatos/TiposDeDatos.cpp
@@ -7,51 +7,43 @@
 #include <assert.h>
 using namespace std;
 
-void testValue(bool booleano)
-{
-  assert(booleano);
-  printf("bool OK\n");
-}
-void testValue(unsigned edad)
-{
-  assert(edad == 21);
-  printf("unsigned OK\n");
-}
-void testValue(int nota)
-{
-  assert(nota == 10);
-  printf("int OK\n");
-}
-void testValue(double promedio)
-{
-  assert(promedio == 10.00);
-  printf("double OK\n");
-}
 void testValue(char arroba)
 {
   assert(arroba == 64);
-  printf("char OK\n");
 }
 void testValue(string mail)
 {
   assert(mail == "dario.20398@gmail.com");
-  printf("string OK\n");
+}
+void testValue(unsigned edad)
+{
+  assert(edad == 21);
+}
+void testValue(int nota)
+{
+  assert(nota == 10);
+}
+void testValue(double promedio)
+{
+  assert(promedio == 10.00);
+}
+void testValue(bool booleano)
+{
+  assert(booleano);
 }
 
 int main() {
-    printf("\nHola de nuevo.\n\n");
-    bool aprobado = true;
-    testValue(aprobado);
+    char arroba = '@';
+    testValue(arroba);
+    string mail = "dario.20398gmail.com";
+    mail.insert(11, 1, arroba);
+    testValue(mail);
     unsigned edad = 21;
     testValue(edad);
     int nota = 10;
     testValue(nota);
     double promedio = 10.00;
     testValue(promedio);
-    char arroba = '@';
-    testValue(arroba);
-    string mail = "dario.20398gmail.com";
-    mail.insert(11, 1, arroba);
-    testValue(mail);
-    printf("\nAdios.\n");
+    bool aprobado = true;
+    testValue(aprobado);
 }
